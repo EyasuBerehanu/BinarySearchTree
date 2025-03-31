6716c0617d5577870f817d8230bab9d6728a3e9f
EyasuBerehanu <eyasoup@gmail.com>
2025-03-31 06:46:38 +0100

    Binary Search Tree ft. Comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+//Eyasu Berehanu
+//3/30/2025
+//This is a binary search tree which allows you to insert values, read values from a file, delte values, and search up values in the bianry search tree 
+//Sources:
+//Some help from my brother with getsuccessor, 
+//Geeksforgeeks on helping how to learn how to do insert, search and delte 
+//Mr Galbrith assistances on print from past heap project
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -7,8 +14,8 @@ using namespace std;
 
 struct Node {
     int data;
-    Node* left;
-    Node* right;
+    Node* left; //left child pointer
+    Node* right; //right child pointer
 
     Node(int value) {
         data = value;
@@ -24,12 +31,12 @@ Node* remove(Node* root, int value);
 Node* getSuccessor(Node* cur); 
 Node* search(Node* root, int value);
 
-Node* insert(Node* root, int value) {
+Node* insert(Node* root, int value) { //insterts value into tree using the logic for Binary search tree
     if (root == nullptr) {
         return new Node(value);
     }
 
-    if (value < root->data) {
+    if (value < root->data) { //uses recurtion to find the right postion for the value wanted for insert
         root->left = insert(root->left, value);
     } else {
         root->right = insert(root->right, value);
@@ -38,8 +45,8 @@ Node* insert(Node* root, int value) {
     return root;
 }
 
-Node* getSuccessor(Node* cur) {
-    if (cur == nullptr || cur->right == nullptr){
+Node* getSuccessor(Node* cur) { //finds the next large node or the successor of a node or number
+    if (cur == nullptr || cur->right == nullptr){ //whyen the current node or right clid is a null then there is no succesor
        return nullptr; 
     } 
     cur = cur->right;
@@ -49,32 +56,32 @@ Node* getSuccessor(Node* cur) {
     return cur;
 }
 
-Node* remove(Node* root, int value) {
+Node* remove(Node* root, int value) { //removes node that was ask to be removed in the bianry search tree
     if (root == nullptr) {
         return nullptr;
     }
 
-    if (value < root->data) {
-        root->left = remove(root->left, value);
-    } else if (value > root->data) {
-        root->right = remove(root->right, value);
+    if (value < root->data) { 
+        root->left = remove(root->left, value); //uses recurtion to remove the node from left if smaller value
+    } else if (value > root->data) { 
+        root->right = remove(root->right, value); //uses recurtion to remove the node from right if larger value
     } else {
         if (root->left == nullptr && root->right == nullptr) {
             delete root;
             return nullptr;
         }
-        else if (root->left == nullptr) {
+        else if (root->left == nullptr) { //replaces root with right child if need to 
             Node* temp = root->right;
             delete root;
             return temp;
         }
-        else if (root->right == nullptr) {
+        else if (root->right == nullptr) { //replaces root with left child if need to 
             Node* temp = root->left;
             delete root;
             return temp;
         }
         else {
-            Node* successor = getSuccessor(root);
+            Node* successor = getSuccessor(root); //replaces with the succersor if both exist inorder succersor
         if (successor != nullptr) {  
                 root->data = successor->data;
                 root->right = remove(root->right, successor->data);
@@ -84,29 +91,29 @@ Node* remove(Node* root, int value) {
     return root;
 }
 
-Node* search(Node* root, int value) {
+Node* search(Node* root, int value) { //searchs for a value in binary search tree
     if (root == nullptr || root->data == value) {
         return root;
     }
 
     if (value > root->data) {
-        return search(root->right, value);
+        return search(root->right, value); //uses recurtyion to seatch the right if value we are searching for is larger
     }
 
-    return search(root->left, value);
+    return search(root->left, value); //uses recurtyion to seatch the left if value we are searching for is smaller
 }
 
-void printTree(Node* root, int depth = 0) {
+void printTree(Node* root, int depth = 0) { //prints tree similar to heap
     if (root == nullptr){
        return; 
     } 
 
-    printTree(root->right, depth + 1);
+    printTree(root->right, depth + 1); //uses recurtion to print right sideof a tree
     for (int i = 0; i < depth; i++) {
         cout << "   ";
     }
     cout << root->data << endl;
-    printTree(root->left, depth + 1);
+    printTree(root->left, depth + 1);  //uses recurtion to print left sideof a tree
 }
 
 void filess(Node*& root) {
@@ -129,10 +136,10 @@ int main() {
     cout << "2 for manual input" << endl;
     cin >> choice;
 
-    if (choice == 1) {
+    if (choice == 1) { //if 1 is chosen then it goes to numbers.txt and prints those numbers
         filess(root);
-    } else if (choice == 2) {
-        cout << "Enter numbers and type done to finish it" << endl;
+    } else if (choice == 2) {  //if 2 is chosen then it lets u imput your own order of numbers
+        cout << "Enter numbers and type done to finish it (only nums from 1-999 plz)" << endl;
         while (true) {
             char input[10];
             cin >> input;
@@ -149,7 +156,9 @@ int main() {
     while (true) {
         cout << " " << endl;
         
-        cout << " " << endl;
+        cout << " " << endl; 
+        
+        //prompts to seach insert remove and quit
         cout << "Type I to insert a number" << endl;
         cout << "Type R to remove a number" << endl;
         cout << "Type S to search for a number" << endl;
@@ -159,19 +168,19 @@ int main() {
         char input[10];
             cin >> input;
             
-        if (strcmp(input, "I") == 0) {
+        if (strcmp(input, "I") == 0) { //if I is types it ask for a value to insert
             cout << "Enter number to insert ";
             cin >> value;
             if (value >= 1 && value <= 999) {
                 root = insert(root, value);
                 printTree(root);
             }
-        } else if (strcmp(input, "R") == 0) {
+        } else if (strcmp(input, "R") == 0) { //if R is types it ask for a value to remove
             cout << "Enter number to remove: ";
             cin >> value;
             root = remove(root, value);
             printTree(root);
-        } else if (strcmp(input, "S") == 0) {
+        } else if (strcmp(input, "S") == 0) { //if S is types it ask for a value to Search
             cout << "Enter number to search for ";
             cin >> value;
             
@@ -184,7 +193,7 @@ int main() {
                 cout << value << " is not in the tree" << endl;
             } 
         } else {
-            break;
+            break; //anything else it breaks
         }
     }
 
